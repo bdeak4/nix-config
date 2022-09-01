cb19f49cdf8d85d85bfe4370e795b3b9287c96cb
Bartol Deak <bdeak@extensionengine.com>
2022-09-01 13:35:00 +0200

    extnsn mac flake.nix

--- extnsn-mbp/darwin-configuration.nix
+++ extnsn-mbp/darwin-configuration.nix
@@ -3,6 +3,8 @@
 {
   imports = [ <home-manager/nix-darwin> ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   users.users.eedev = {
     name = "eedev";
     home = "/Users/eedev";
@@ -14,13 +16,9 @@
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages =
-    [ pkgs.vim
-    ];
-
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+  environment.systemPackages = with pkgs; [
+    vim
+  ];
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
