20532f1d42edce76889ffb4761538e885213be16
Bartol Deak <b@bdeak.net>
2022-09-14 08:06:09 +0200

    rm wayland=false

--- systems/thinkpad-home/configuration.nix
+++ systems/thinkpad-home/configuration.nix
@@ -19,7 +19,6 @@
   services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
-    displayManager.gdm.wayland = false;
     desktopManager.gnome.enable = true;
     layout = "us";
   };
